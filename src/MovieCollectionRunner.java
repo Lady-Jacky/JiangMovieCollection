b58d437b58eaabc6a5b1652b52f16f5d838327a4
1E10 Laptop <sample@bths.edu>
2023-02-08 11:03:01 -0500

    Initial commit

---
+++ src/MovieCollectionRunner.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class MovieCollectionRunner {
+  public static void main(String[] args) {
+    /* TASK 1: finish the code for the importMovieList helper method
+       in the MovieCollection class, then write some test code here to create
+       a new MovieCollection object from the movies in the movies_data.csv file,
+       get the movies arraylist, and print out each movie.
+       use the code in the CerealCollectionRunner as an example. */
+
+
+
+  }
+}
